b51f59cfb79c8051b684d5b393b8e13ef31fc30c
Óscar de Arriba <odarriba@gmail.com>
2014-10-18 20:36:52 +0200

    Added some tests for User model, but error raises
    ADD: Some User model tests
    Added some User model tests, but it fails before testing due to
    some error with fixtures, maybe related to MongoID.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without email" do
+    user = User.new(:password => "testtest", :password_confirmation => "testtest", :name => "Test User", :level => User::ADMINISTRATOR_USER, :lang => I18n.default_locale)
+    assert_not user.save
+  end
+
+  test "should not save user without a valid email" do
+    user = User.new(:email => "hey", :password => "testtest", :password_confirmation => "testtest", :name => "Test User", :level => User::ADMINISTRATOR_USER, :lang => I18n.default_locale)
+    assert_not user.save
+  end
+
+  test "should not save user without name" do
+    user = User.new(:email => "hey@test.org", :password => "testtest", :password_confirmation => "testtest", :level => User::ADMINISTRATOR_USER, :lang => I18n.default_locale)
+    assert_not user.save
+  end
 end
